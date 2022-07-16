fbf0960ed9af468bc89f5f32e5fac719da243028
marcosreis7 <mreis6357133@gmail.com>
2022-07-16 03:09:29 -0300

    corrigindo erros de pastas

--- Pizzaria.dpr
+++ Pizzaria.dpr
@@ -2,10 +2,10 @@ program Pizzaria;
 
 uses
   Vcl.Forms,
-  Tela_inicial in '..\Tela_inicial.pas' {frm_tela1},
-  Tela_2 in '..\Tela_2.pas' {frm_tela2},
-  Tela_3 in '..\Tela_3.pas' {frm_tela3},
-  Tela_4 in 'Tela_4.pas' {Tela_calculadora};
+  Tela_2 in 'Tela_2.pas' {frm_tela2},
+  Tela_3 in 'Tela_3.pas' {frm_tela3},
+  Tela_4 in 'Tela_4.pas' {Tela_calculadora},
+  Tela_inicial in 'Tela_inicial.pas' {frm_tela1};
 
 {$R *.res}
 
@@ -13,6 +13,5 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(Tfrm_tela1, frm_tela1);
-  Application.CreateForm(TTela_calculadora, Tela_calculadora);
   Application.Run;
 end.
